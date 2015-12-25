e173dd9920eb1cad7ab944063b8104c2d4517407
sonallux <13821543+sonallux@users.noreply.github.com>
2015-12-25 19:45:30 +0100

    Changed BooleanConverter to match test cases.

--- src/test/java/jsone_studios/settings/BooleanConverterTest.java
+++ src/test/java/jsone_studios/settings/BooleanConverterTest.java
@@ -1,25 +1,25 @@
 package jsone_studios.settings;
 
-import static jsone_studios.settings.Converters.*;
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static jsone_studios.settings.Converters.BOOLEAN_CONVERTER;
+import static org.junit.Assert.*;
 
 public class BooleanConverterTest {
 
     @Test
     public void testBooleanFromString() {
-        //TODO: 0 und 1 in string does not convert to false / true
         assertTrue(BOOLEAN_CONVERTER.fromString("true"));
         assertTrue(BOOLEAN_CONVERTER.fromString("True"));
         assertTrue(BOOLEAN_CONVERTER.fromString("TRUE"));
         assertTrue(BOOLEAN_CONVERTER.fromString("tRuE"));
-        //assertTrue(BOOLEAN_CONVERTER.fromString("1"));
+        assertTrue(BOOLEAN_CONVERTER.fromString("1"));
 
         assertFalse(BOOLEAN_CONVERTER.fromString("false"));
         assertFalse(BOOLEAN_CONVERTER.fromString("False"));
         assertFalse(BOOLEAN_CONVERTER.fromString("FALSE"));
         assertFalse(BOOLEAN_CONVERTER.fromString("fAlSe"));
-        //assertFalse(BOOLEAN_CONVERTER.fromString("0"));
+        assertFalse(BOOLEAN_CONVERTER.fromString("0"));
 
         assertFalse(BOOLEAN_CONVERTER.fromString("asdf"));
         assertFalse(BOOLEAN_CONVERTER.fromString(null));
